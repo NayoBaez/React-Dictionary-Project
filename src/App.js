2c0bfee979c963a6a5b13010fd3f6a5ce1e66ce5
NayoBaez <104393006+NayoBaez@users.noreply.github.com>
2022-08-13 15:55:22 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi World!</h1>
+      <h2> I am about to create a React Dictionary APP</h2>
+      <button className="btn btn-primary">Button</button>
+    </div>
+  );
+}
+
+export default App;
