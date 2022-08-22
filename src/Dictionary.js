8b292d8f58dc52895aa0931eee242dadfc2b18a4
NayoBaez <104393006+NayoBaez@users.noreply.github.com>
2022-08-22 18:29:28 -0400

    Integrated Pexels Api & Created Photos Component
    Integrated Pexels Api to display images in Photos component, which displays images for the dictionary keyword.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 import "./Dictionary.css";
 
@@ -8,14 +9,25 @@ export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
 
   function Search() {
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    let dictionaryApiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(dictionaryApiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001f2ddcd4d8c2040a1a5a92d54eeab8675";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let header = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: header }).then(handlePexelsResponse);
   }
 
   function HandleSubmit(event) {
@@ -45,10 +57,11 @@ export default function Dictionary(props) {
             />
           </form>
           <div className="hint">
-            e.g. Beach, Sunset, Coconut, Paragliding, Giraffe
+            e.g. Beach, Palmtree, Coconut, Giraffe, Paragliding, Adventure
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,12 +2,10 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  console.log(props.phonetic);
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">
-        <i class="fa-solid fa-volume-high"></i>
-        {""} {""} Listen
+        <i className="fa-solid fa-volume-high"></i> {""} {""} Listen
       </a>
       <span className="text">{props.phonetic.text}</span>
     </div>
