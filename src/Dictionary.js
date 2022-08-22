1ab8f544a684c91fdb655ca926a1f1004a64b65d
NayoBaez <104393006+NayoBaez@users.noreply.github.com>
2022-08-22 13:41:08 -0400

    Refactored Dictionary for default keyword
    Refactored search function in Dictionary component to have a default keyword on load

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,36 +4,50 @@ import Results from "./Results";
 
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function Search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function HandleSubmit(event) {
+    event.preventDefault();
+    Search();
   }
 
   function handleKeyword(event) {
     return setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <section>
-        <form onSubmit={search}>
-          <input type="search" onChange={handleKeyword} />
-        </form>
-        <div className="hint">
-          e.g. Beach, Sunset, Coconut, Paragliding, Giraffe
-        </div>
-      </section>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    Search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={HandleSubmit}>
+            <input type="search" onChange={handleKeyword} />
+          </form>
+          <div className="hint">
+            e.g. Beach, Sunset, Coconut, Paragliding, Giraffe
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
